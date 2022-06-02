a1f1fb5dfb0fe5074f00255c7f2cd5750b0ee93d
Elías <solerurban.elias@gmail.com>
2022-06-02 18:33:59 +0200

    [aulaSQL-14] feat: welcome page

--- laravel_app/resources/views/layouts/app.blade.php
+++ laravel_app/resources/views/layouts/app.blade.php
@@ -8,10 +8,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>aulaSQL</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
