7cde9e8aedf1f98523e259164f510e0a20481205
Elías <solerurban.elias@gmail.com>
2022-06-07 18:10:32 +0200

    [aulaSQL-14] styles: add student alert

--- laravel_app/resources/views/add_student.blade.php
+++ laravel_app/resources/views/add_student.blade.php
@@ -1,22 +1,26 @@
 @extends('static')
 @section('content')
-<div class="add-student-container">
-    <h2>{{$classroom->name}}</h2>
-    <form class="form-inline" method="post" action={{ route('add_student_to_classroom', $classroom->id) }}>
-        @csrf
-        <input class="mr-2" type="text" name="student_email" placeholder="Email del alumno">
-        <input type="hidden" name="classroom_id" value={{ $classroom->id }}>
-        <button type="submit" class="btn teacher-btn">Añadir</button>
-    </form>
-    @if(session('ok'.$classroom->id))
-        {{-- //TODO: alert: https://getbootstrap.com/docs/4.0/components/alerts/ --}}
-        <script>window.alert(@json(session('ok'.$classroom->id)))</script>
-    @endif
-    @if(session('ko'.$classroom->id))
-        {{-- //TODO: alert: https://getbootstrap.com/docs/4.0/components/alerts/ --}}
-        <script>window.alert(@json(session('ko'.$classroom->id)))</script>
-    @endif
-</div>
-
+    <div class="add-student-container">
+        <h2>{{ $classroom->name }}</h2>
+        <form class="form-inline" method="post" action={{ route('add_student_to_classroom', $classroom->id) }}>
+            @csrf
+            <input class="mr-2" type="text" name="student_email" placeholder="Email del alumno">
+            <input type="hidden" name="classroom_id" value={{ $classroom->id }}>
+            <button type="submit" class="btn teacher-btn">Añadir</button>
+        </form>
+        @if (session('ok' . $classroom->id))
+            <div class="alert alert-success mt-4" role="alert">
+                <p>{{ @session('ok' . $classroom->id) }}</p>
+            </div>
+        @endif
+        @if (session('ko' . $classroom->id))
+            <div class="alert alert-danger mt-4" role="alert">
+                <p>{{ @session('ko' . $classroom->id) }}</p>
+            </div>
+    
+            {{-- <script>
+                window.alert(@json(session('ko' . $classroom->id)))
+            </script> --}}
+        @endif
+    </div>
 @endsection
-
